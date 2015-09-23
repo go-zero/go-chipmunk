1e1d2a1709fc51d309b1aa06a58191a81e953d40
Jairo Luiz <t4ngz3r0@gmail.com>
2015-09-23 16:12:45 -0300

    some docs

--- space.go
+++ space.go
@@ -11,17 +11,20 @@ type Space struct {
 	ptr *C.cpSpace
 }
 
+// Allocate and initialize a Space.
 func NewSpace() *Space {
 	space := new(Space)
 	space.ptr = C.cpSpaceNew()
 	return space
 }
 
+// Get the gravity passed to rigid bodies when integrating velocity.
 func (s *Space) GetGravity() (float64, float64) {
 	vect := C.cpSpaceGetGravity(s.ptr)
 	return float64(vect.x), float64(vect.y)
 }
 
+// Set the gravity to pass to rigid bodies when integrating velocity.
 func (s *Space) SetGravity(x float64, y float64) {
 	C.cpSpaceSetGravity(s.ptr, C.cpVect{C.cpFloat(x), C.cpFloat(y)})
 }
